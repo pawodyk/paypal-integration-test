fad76f0c75f51e1143e2bfa30551e6dd1b388fb3
Pawel <14218944+pawodyk@users.noreply.github.com>
2023-07-07 15:30:34 +0100

    add checkout

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Integration of PayPal Payment using braintree API
+
+Step 1
+Your front-end requests a client token from your server and initializes the client SDK.
+
+Step 2
+Your server generates and sends a client token back to your client using the server SDK.
+
+Step 3
+The customer submits payment information, the client SDK communicates that information to Braintree and returns a payment method nonce.
+
+Step 4
+Your front-end sends the payment method nonce to your server.
+
+Step 5
+Your server code receives the payment method nonce and then uses the server SDK to create a transaction.
--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+import braintree
+
+
+gateway = braintree.BraintreeGateway(
+  braintree.Configuration(
+      braintree.Environment.Sandbox,
+      merchant_id="use_your_merchant_id",
+      public_key="use_your_public_key",
+      private_key="use_your_private_key"
+  )
+)
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def home():
+  return render_template('index.html')
+
+@app.get("/checkout")
+def render_checkout():
+  return render_template("checkout.html")
+
+@app.post("/checkout")
+def process_checkout():
+  return "<p> checkout completed"
