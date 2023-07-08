130540192aca2778d706b952bcae336d3c0f9c70
Pawel <14218944+pawodyk@users.noreply.github.com>
2023-07-08 14:54:15 +0100

    payment integration and introduced dimple templates

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request, url_for, redirect
 import braintree
+import os
 
 
 gateway = braintree.BraintreeGateway(
   braintree.Configuration(
-      braintree.Environment.Sandbox,
-      merchant_id="use_your_merchant_id",
-      public_key="use_your_public_key",
-      private_key="use_your_private_key"
+    environment=braintree.Environment.Sandbox,
+    merchant_id=os.getenv("MERCHANT_ID"),
+    public_key=os.getenv("PUBLIC_KEY"),
+    private_key=os.getenv("PRIVATE_KEY")
   )
 )
+
 
 
 app = Flask(__name__)
@@ -22,6 +24,29 @@ def home():
 def render_checkout():
   return render_template("checkout.html")
 
-@app.post("/checkout")
-def process_checkout():
-  return "<p> checkout completed"
+@app.post("/payment")
+def process_payment():
+  payload = request.get_json()
+  print(payload)
+  print(payload["nonce"], payload["type"])
+  try:
+    result = gateway.transaction.sale({
+        "amount": "10.00",
+        "payment_method_nonce": payload["nonce"],
+        "options": {
+          "submit_for_settlement": True
+        }
+    })
+    if result.is_success:
+      print("successful payment", result.transaction.id)
+    else:
+      print(result)
+
+  except Exception as ex:
+    print(ex)
+
+  return render_template("checkout.html") # redirect(url_for("render_checkout"))
+
+@app.route("/refund")
+def refund_payment():
+  return redirect(url_for("home"))
