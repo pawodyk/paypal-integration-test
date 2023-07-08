6ba9490dd5aa5b587e7cf02f08da7e3add20cebd
Pawel <14218944+pawodyk@users.noreply.github.com>
2023-07-08 18:31:16 +0100

    added refund functionality

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def render_checkout():
 @app.post("/payment")
 def process_payment():
   payload = request.get_json()
-  print(payload)
-  print(payload["nonce"], payload["type"])
+  # print(payload)
+  # print(payload["nonce"], payload["type"])
   try:
     result = gateway.transaction.sale({
         "amount": "10.00",
@@ -39,14 +39,51 @@ def process_payment():
     })
     if result.is_success:
       print("successful payment", result.transaction.id)
+      # return redirect(url_for(payment_success))
     else:
-      print(result)
+      print("this: ", result)
+    return {"status": "success", "transactionID": result.transaction.id}, 201
 
   except Exception as ex:
     print(ex)
+    return "exception", 500 
+  
 
-  return render_template("checkout.html") # redirect(url_for("render_checkout"))
 
-@app.route("/refund")
+@app.get("/refund")
+def refund_page():
+  return render_template("refund.html")
+
+@app.post("/refund")
 def refund_payment():
-  return redirect(url_for("home"))
+  transactionID = request.form.get("transactionID")
+  data = {}
+  try:
+    result = gateway.transaction.refund(transactionID)
+
+    if result.is_success:
+      print(result.transaction.status)
+      print(result.transaction.processor_response_code)
+      print(result.transaction.processor_response_text)
+      data["status"] = "success"
+
+    else:
+      '''
+      result.is_success
+      # false                             |   # false
+
+      result.transaction.status
+      # "processor_declined"              |   # "settlement_declined"
+
+      result.transaction.processor_response_code
+      # e.g. "2005"                       |  # e.g. "4001"
+
+      result.transaction.processor_response_text
+      # e.g. "Invalid Credit Card Number" |  # e.g. "Settlement Declined"
+      '''
+      pass
+  except Exception as ex:
+    data["status"] = "fail"
+    data['Error'] = ex
+    
+  return render_template("refund.html", data = data)
